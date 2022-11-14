dfb1a85d0d82351d6afcc683fa4dce2ca95a449c
Вадим <93205098+LXXSE@users.noreply.github.com>
2022-11-14 23:31:32 +0700

    Update Practice6.1.js

--- Practice6.1.js
+++ Practice6.1.js
@@ -3,7 +3,6 @@ var a = 0
 var b = 0
 for (i=1;i<=10;++i){roadMines.push(Math.floor(Math.random() * (2)))}
 for (i = 1; i <= roadMines.length; ++i){
-
     if (roadMines[i] == 1) {
         a = a + 1
         b = b + 1 }
@@ -15,6 +14,5 @@ for (i = 1; i <= roadMines.length; ++i){
     if (a==3) {
         console.log('Танк уничтожен')
         break } 
-    if (i==10) {console.log('Танк дошёл до Берлина! Ура! Победа!')}
-    
+    if (i==10) {console.log('Танк дошёл до Берлина! Ура! Победа!')}   
 }
